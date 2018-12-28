032dcbed562bc581c8ac51ef9577383e3c4f379a
boomshakakan <msteve20@uic.edu>
2018-12-28 00:33:48 -0600

    Attempting python methods

--- src/switchController.py
+++ src/switchController.py
@@ -1,13 +1,48 @@
+# External Modules
 import RPi.GPIO as GPIO
+import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
 
-# could optionally be GPIO.LOW / 0 / false
-# out_List = {11,12}
-GPIO.output(12, GPIO.LOW)
 
-GPIO.output(out_List, GPIO.LOW)
+
+def config():
+
+	# follows pin numbers listed on header P1
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(pin_number, GPIO.OUT)
+
+	# could optionally be GPIO.LOW / 0 / false
+	# out_List = {11,12}
+	pin_number = 11
+	GPIO.output(pin_number, GPIO.LOW)
+
+	# WE CANNOT DO THIS SINCE WE CANNOT TRIGGER MULTIPLE AT THE SAME TIME 
+	# GPIO.output(out_List, GPIO.LOW)
+
+	# INSTEAD WE MUST WRITE A FUNCTION THAT TAKES AS INPUT THE PIN TO SET LOW
+	# WE THEN SET THE PIN LOW AND CALL A FUNCTION TO WAIT AND REPEAT
+	return
+
+def wait():
+	time.sleep(0.075)
+	return
+
+def clean():
+	GPIO.cleanup()
+	return
+
+def main():
+print("Program Running...")
+
+config()
+wait()
+clean()
+return
+
+
+
+
+
 
 
 
